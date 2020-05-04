410532c2b6de23fa40321486062a71d0a078dc62
Денис Виталиевич <denisvitalievich5@gmail.com>
2020-05-05 00:37:27 +0300

    Simple Security configuration

---
+++ src/main/java/com/project/gym/Configs/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.project.gym.Configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select email as principal, password as credentails, true from users where email=?")
+                .authoritiesByUsernameQuery("select user_email as principal, role_name as role from user_roles where user_email=?")
+                .passwordEncoder(passwordEncoder()).rolePrefix("ROLE_");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // TODO Auto-generated method stub
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/register", "/", "/about", "/login", "/css/**", "/webjars/**").permitAll()
+                .antMatchers("/profile").hasAnyRole("USER,ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and().formLogin().loginPage("/login").permitAll()
+                .defaultSuccessUrl("/profile").and().logout().logoutSuccessUrl("/login");
+    }
+}
--- src/main/java/com/project/gym/Services/WeightService.java
+++ src/main/java/com/project/gym/Services/WeightService.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class WeightService {
+public class WeightService{
     @Autowired
     private WeightRepository weightRepository;
 
